424a43d75b0d782e01cbfec21d5acde5f578e6ed
unknown <nast.danilova2002@yandex.ru>
2025-03-23 21:05:59 +0300

    Added new func

--- lab02/lab02.py
+++ lab02/lab02.py
@@ -12,22 +12,22 @@ class VideoRemaker():
         self.clip = clip
 
     # сделаем функцию высшего порядка
-    def change_video_while_playing(self,red,green,blue):
+    def change_video_while_playing(self, clip, red, green,blue, intns, area_size, funcNum):
         # будем собирать фреймы видео пошагово
         # на основе проходящих через фильтр кадров
         new_frames = []
         # разберём текущее видео на фреймы
-        frames = [frame for frame in self.clip.iter_frames()]
+        frames = [frame for frame in clip.iter_frames()]
         for i, frame in enumerate(frames):
             print(f"Работаю с фреймом {i}")
             # Получим время фрейма в сек.
-            time = i / self.clip.fps
+            time = i / clip.fps
             # превратим цифры в кадр изображения
             image = Image.fromarray(frame)
-            after_filter = self.__change_func(image, time,red,green,blue)
+            after_filter = self.__change_func(image, time,red,green,blue, intns, area_size, funcNum)
             # кладём изображение после фильтра в new_frames
             new_frames.append(np.array(after_filter))
-        new_clip = ImageSequenceClip(new_frames, fps=self.clip.fps)
+        new_clip = ImageSequenceClip(new_frames, fps=clip.fps)
         return new_clip
 
     def save_clip(self, clip, dstn):
@@ -35,8 +35,17 @@ class VideoRemaker():
         os.makedirs(os.path.dirname(dstn), exist_ok=True)
         clip.write_videofile(dstn, codec="libx264")
 
-    def __change_func(self, image: Image, time: int,red,green,blue) -> Image:
+    def __change_func(self, image: Image, time: int,red,green,blue, intns, area_size, funcNum) -> Image:
         remaker = l1.ImageRemaker(image)
         time*=10
-        filtered_image = remaker.color_correction(red=red * time, green=green * time, blue=blue * time)
+        if funcNum == 1:
+            filtered_image = remaker.color_correction(red=red * time, green=green * time, blue=blue * time)
+        elif funcNum == 2:
+            filtered_image = remaker.add_noise(intns * time)
+        elif funcNum == 3:
+            filtered_image = remaker.monotone(area_size * time)
         return filtered_image
+        
+    def combination(self, clip2: VideoFileClip,red,green, blue, intns, area_size):
+        clip1 = self.change_video_while_playing(self.clip, red, green, blue, intns, area_size, 2)
+        clip2 = self.change_video_while_playing(clip2, red, green, blue, intns, area_size, 3)
