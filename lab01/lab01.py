d7d3ab546bd95beef3a6aa3a502b5d0c54f3dfcd
unknown <nast.danilova2002@yandex.ru>
2025-03-23 19:47:14 +0300

    Changed func signatures

--- lab01/lab01.py
+++ lab01/lab01.py
@@ -27,28 +27,31 @@ class ImageRemaker:
         for w in range(self.width):
             for h in range(self.height):
                 r, g, b = pixels[w, h]
+                # if h%50==0:
+                #     print(f"old pixels {pixels[w,h]}")
                 # увеличиваем насыщение
                 r = int(r * red)
                 g = int(g * green)
                 b = int(b * blue)
+                # if h % 50 == 0:
+                #     print(f"new_pixels {r},{g},{b}")
                 # проверка чтоб не убежали за максимальное насыщение (255)
                 r = min(r, 255)
                 g = min(g, 255)
                 b = min(b, 255)
                 pixels[w, h] = r, g, b
 
-        # сохраняем изображение в папку
-        self.save_image(corrected_image, "color_corrected.jpg")
+        return corrected_image
 
     # Накладываем шум на изображение
     def add_noise(self, intns: int):
         # Опять же - создаем копию изображения и работаем с ней
         new_image = self.image.copy()
         pixels = new_image.load()
-        # Опять же - картинка состоит из пикселей, представленных в виде матрицы, поэтому пробегаемся по матрице и меняем
+        # Опять же - картинка состоит из пикселей, представленных в виде матрицы,
+        # поэтому пробегаемся по матрице и меняем
         # пиксели
         for w in range(self.width):
-
             for h in range(self.height):
                 # берём текущие пиксели
                 r, g, b = pixels[w, h]
@@ -62,7 +65,41 @@ class ImageRemaker:
                 # Меняем изначальные пиксели изображения на новые
                 pixels[w, h] = r, g, b
 
-        # сохраняем новую картинку в папке
-        self.save_image(new_image, "noisy.jpg")
-    def Sepia(self, r: float, g: float, b: float):
-        pass
+        return new_image
+
+    # Монотонность на изображении
+
+    # Что мы в принципе хотим?
+    # Выбираем область размера area
+    # внутри области уменьшаем значение каждого из цветов в соотв. с усред. значением
+    def monotone(self, area_size):
+        new_image = self.image.copy()
+        pixels = new_image.load()
+
+        half_size = area_size // 2
+        # избегаем выхода за пределы краёв фотки
+        for w in range(half_size, self.width - half_size):
+            for h in range(half_size, self.height - half_size):
+                # Собираем значения пикселей в области
+                r_sum, g_sum, b_sum = 0, 0, 0
+                count = 0
+                # поищем все пиксели вокруг центрального. Для примера в area=3 получаем
+                # (w - 1, h - 1) | (w, h - 1) | (w + 1, h - 1)
+                # (w - 1, h)     |   (x, h)   | (w + 1, h)
+                # (w - 1, h + 1) | (w, h + 1) | (w + 1, h + 1)
+                for i in range(-half_size, half_size + 1):
+                    for j in range(-half_size, half_size + 1):
+                        r, g, b = pixels[w + i, h + j]
+                        r_sum += r
+                        g_sum += g
+                        b_sum += b
+                        count += 1
+                # Усредняем значения
+                new_r = r_sum // count
+                new_g = g_sum // count
+                new_b = b_sum // count
+
+                # Присваиваем усредненное значение центральному пикселю
+                pixels[w, h] = (new_r, new_g, new_b)
+
+        return new_image
