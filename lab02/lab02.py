2976cdac4286ff271bcfb535e7712f9c0405be66
unknown <nast.danilova2002@yandex.ru>
2025-03-24 05:13:03 +0300

    Changed libs for fx

--- lab02/lab02.py
+++ lab02/lab02.py
@@ -1,32 +1,32 @@
 import os
-
 import numpy as np
 
 from lab01 import lab01 as l1
-from moviepy import VideoFileClip, ImageSequenceClip, CompositeVideoClip, vfx
+from moviepy.editor import VideoFileClip, CompositeVideoClip, vfx, ImageSequenceClip
 from PIL import Image
-
 
 class VideoRemaker():
     def __init__(self, clip: VideoFileClip):
         self.clip = clip
 
     # сделаем функцию высшего порядка
-    def change_video_while_playing(self, clip, red, green,blue, intns, area_size, funcNum):
+    def change_video_while_playing(self, clip, red, green, blue, intns, area_size, funcNum):
         # будем собирать фреймы видео пошагово
         # на основе проходящих через фильтр кадров
         new_frames = []
         # разберём текущее видео на фреймы
         frames = [frame for frame in clip.iter_frames()]
         for i, frame in enumerate(frames):
-            print(f"Работаю с фреймом {i}")
+            print(f"working {i}")
             # Получим время фрейма в сек.
             time = i / clip.fps
             # превратим цифры в кадр изображения
             image = Image.fromarray(frame)
-            after_filter = self.__change_func(image, time,red,green,blue, intns, area_size, funcNum)
+            after_filter = self.__change_func(image, time, red, green, blue, intns, area_size, funcNum)
             # кладём изображение после фильтра в new_frames
             new_frames.append(np.array(after_filter))
+        
+        # Создаем ImageSequenceClip из кадров
         new_clip = ImageSequenceClip(new_frames, fps=clip.fps)
         return new_clip
 
@@ -35,21 +35,23 @@ class VideoRemaker():
         os.makedirs(os.path.dirname(dstn), exist_ok=True)
         clip.write_videofile(dstn, codec="libx264")
 
-    def __change_func(self, image: Image, time: int,red,green,blue, intns, area_size, funcNum) -> Image:
+    def __change_func(self, image: Image, time: int, red, green, blue, intns, area_size, funcNum) -> Image:
         remaker = l1.ImageRemaker(image)
-        time*=10
+        time *= 10
         if funcNum == 1:
             filtered_image = remaker.color_correction(red=red * time, green=green * time, blue=blue * time)
         elif funcNum == 2:
-            filtered_image = remaker.add_noise(intns * time)
+            filtered_image = remaker.add_noise(int(intns * time))
         elif funcNum == 3:
-            filtered_image = remaker.monotone(area_size * time)
+            filtered_image = remaker.monotone(int(area_size * time))
         return filtered_image
-        
-    def combination(self, clip2: VideoFileClip,red,green, blue, intns, area_size):
+
+    def combination(self, clip2: VideoFileClip, red, green, blue, intns, area_size):
+        # Обработка первого клипа
         clip1 = self.change_video_while_playing(self.clip, red, green, blue, intns, area_size, 2)
-        clip2 = self.change_video_while_playing(clip2, red, green, blue, intns, area_size, 3)
-        crossfade_duration = 1  
+        # Обработка второго клипа
+        clip2 = self.change_video_while_playing(clip2, red, green, blue, intns, area_size, 1)
+        crossfade_duration = 1  # Например, 1 секунда
 
         # Применяем эффект fadeout к первому клипу
         clip1_fadeout = clip1.fx(vfx.fadeout, crossfade_duration)
@@ -65,4 +67,4 @@ class VideoRemaker():
         ])
 
         # Сохраняем результат
-        final_clip.write_videofile("output_crossfade.mp4", fps=24)
+        final_clip.write_videofile("output_crossfade.mp4")
--- main.py
+++ main.py
@@ -1,28 +1,31 @@
-from lab01 import lab01 as l1
+# from lab01 import lab01 as l1
 from lab02 import lab02 as l2
-from moviepy import VideoFileClip, ImageSequenceClip
-from PIL import Image
+from moviepy import VideoFileClip
 
-IMAGE_SOURCE = "test_photo.jpg"
-VIDEO1_SOURCE = "clip.mp4"
+
+# IMAGE_SOURCE = "test_photo.jpg"
+VIDEO1_SOURCE = "clip1s.mp4"
 if __name__ == "__main__":
     # lab1
-    image = Image.open(IMAGE_SOURCE)
-    remaker = l1.ImageRemaker(image)
+    # image = Image.open(IMAGE_SOURCE)
+    # remaker = l1.ImageRemaker(image)
 
-    img1 = remaker.color_correction(red=1.5, green=2.5, blue=1.8)
-    remaker.save_image(img1,"color_corrected.jpg")
+    # img1 = remaker.color_correction(red=1.5, green=2.5, blue=1.8)
+    # remaker.save_image(img1,"color_corrected.jpg")
 
-    img2 = remaker.add_noise(intns=120)
-    remaker.save_image(img2, "noisy.jpg")
+    # img2 = remaker.add_noise(intns=120)
+    # remaker.save_image(img2, "noisy.jpg")
 
-    img3 = remaker.sepia()
-    remaker.save_image(img3, "sepia.jpg")
+    # img3 = remaker.sepia()
+    # remaker.save_image(img3, "sepia.jpg")
 
-    img4 = remaker.monotone(area_size=9)
-    remaker.save_image(img4,"mono_img.jpg")
+    # img4 = remaker.monotone(area_size=9)
+    # remaker.save_image(img4,"mono_img.jpg")
     # lab2
-    # clip = VideoFileClip(VIDEO1_SOURCE)
-    # clip2 = VideoFileClip(VIDEO1_SOURCE)
-    # VideoMaker=l2.VideoRemaker(clip)
-    # VideoMaker.combination(clip2, 0.3, 0.2, 0.4, 100, 2)
+    clip = VideoFileClip(VIDEO1_SOURCE)
+    clip2 = VideoFileClip(VIDEO1_SOURCE)
+    VideoMaker=l2.VideoRemaker(clip)
+    VideoMaker.combination(clip2, 0.3, 0.2, 0.4, 100, 2)
+    # clip1 = VideoMaker.change_video_while_playing(red=1.5, green=2.5, blue=3.8)
+    # VideoMaker.save_clip(clip1, "noisy_vid.mp4")
+  
