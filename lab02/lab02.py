8fd02b87f7fdb3e3b606e33bf4cf454960e43f58
Oleg Gagloev <ogagloev@team.amocrm.com>
2025-03-25 12:03:06 +0300

    тупанул

--- lab02/lab02.py
+++ lab02/lab02.py
@@ -45,7 +45,23 @@ class VideoRemaker():
         elif funcNum == 3:
             filtered_image = remaker.monotone(area_size * time)
         return filtered_image
-        
-    def combination(self, clip2: VideoFileClip,red,green, blue, intns, area_size):
+
+    def __change_func(self, image: Image, time: int, red, green, blue, intns, area_size, funcNum) -> Image:
+        remaker = l1.ImageRemaker(image)
+        time *= 10
+        if funcNum == 1:
+            filtered_image = remaker.color_correction(red=red * time, green=green * time, blue=blue * time)
+        elif funcNum == 2:
+            filtered_image = remaker.add_noise(int(intns * time))
+        elif funcNum == 3:
+            filtered_image = remaker.monotone(int(area_size * time))
+        return filtered_image
+
+    def combination(self, clip2: VideoFileClip, red, green, blue, intns, area_size):
+        # Создаем потоки
+
         clip1 = self.change_video_while_playing(self.clip, red, green, blue, intns, area_size, 2)
-        clip2 = self.change_video_while_playing(clip2, red, green, blue, intns, area_size, 3)
+        clip2 = self.change_video_while_playing(clip2, red, green, blue, intns, area_size, 1)
+        final_clip = page_flip_transition(clip1, clip2, 0.7)
+        # Сохраняем результат
+        final_clip.write_videofile("output_crossfade.mp4", fps=24)
